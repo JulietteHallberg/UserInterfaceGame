049fa0dca09f397ebd5cdae232e1addbafcf65ff
juliettehallberg <juliette.hallberg@gmail.com>
2015-11-27 17:08:08 +0000

    Added game class

---
+++ game.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class game {
+	public static void main (String[] args) throws IOException {
+		
+	//set up file and stream
+	File outFile = new File ("question.txt");
+	FileOutputStream outFileStream = new FileOutStream(outFile);
+	PrintWriter outStream = new PrintWriter(outFileStream);
+}
